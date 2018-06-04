eec3a31f9f2c48c16fdbf6d953f89d166d0e1a0a
Adwiti789 <adwiti1975@gmail.com>
2018-06-04 00:26:56 -0700

    If else condition

---
+++ src/variables/IfElseCondition.scala
@@ -0,0 +1,17 @@
+package variables
+
+/**
+  * Created by Adwiti on 6/4/2018.
+  */
+object IfElseCondition {
+
+  def main(args: Array[String]): Unit = {
+
+    val a = 10;
+    val b = 12;
+
+    val c = if (a > b) a else b
+    print(c)
+  }
+
+}
