c672ebf9925cd2001f37677416ce37e5420bcacb
Adwiti789 <adwiti1975@gmail.com>
2018-06-04 23:50:42 -0700

    For loop

---
+++ src/loops/ForLoop.scala
@@ -0,0 +1,13 @@
+package loops
+
+/**
+  * Created by Adwiti on 6/4/2018.
+  */
+object ForLoop {
+
+  def main(args: Array[String]): Unit = {
+    for (arg <- args) {
+      println(arg)
+    }
+  }
+}
